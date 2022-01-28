96687835dc71fd3673d4487ff7fde4c52e72a996
ilkrclm <ilkercalim@hotmail.com>
2022-01-28 12:51:53 +0300

    tailwind font edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [
-          // 'Quicksand',
+          'Inter',
           ...defaultTheme.fontFamily.sans,
         ]
       },
